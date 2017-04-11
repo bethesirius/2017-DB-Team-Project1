523cedb7d1d1418feef5bfb30b422c4a81b651f3
HanJongKo <rino0601.dev@gmail.com>
2017-04-11 15:45:43 +0900

    Storage Create form 완료

--- react-frontend/src/form/StorageCreateForm.js
+++ react-frontend/src/form/StorageCreateForm.js
@@ -18,6 +18,13 @@ class StorageCreateForm extends React.Component {
     static fieldNames = {
         manage_num: "manage_num",
         spec: "spec",
+        location: "location",
+        new_spec: {
+            disk_spec: "disk_spec",
+            disk_type: "disk_type",
+            spec: "spec",
+            volume: "volume",
+        },
     };
 
     static validate(values) {
@@ -29,7 +36,10 @@ class StorageCreateForm extends React.Component {
         super(props);
         this.state = {
             isFetching: false,
+            locations: [],
             specs: [],
+            spec_names: [],
+            spec_types: [],
         };
     }
 
@@ -38,7 +48,10 @@ class StorageCreateForm extends React.Component {
         this.setState({isFetching: true});
         Promise.all([
             fetch("/api/storage_spec").then(res => res.json()),
-        ]).then(([spec]) => {
+            fetch("/api/detail_location").then(res => res.json()),
+            fetch("/api/storage_spec_type").then(res => res.json()),
+            fetch("/api/storage_spec_name").then(res => res.json()),
+        ]).then(([spec, location, spec_type, spec_name]) => {
             this.setState((state, props) => {
                 state.specs = spec.objects.map(item => {
                     let spec_type = item.disk_type ? item.disk_type.spec_type : 'unknown';
@@ -48,40 +61,75 @@ class StorageCreateForm extends React.Component {
                         value: item.id
                     };
                 });
+                state.locations = location.objects.map(item => {
+                    let detail = item.location.detail ? item.location.detail : '';
+                    return {
+                        text: `${item.location.location}-${detail}`,
+                        value: item.id
+                    };
+                });
+                state.spec_types = spec_type.objects.map(item => {
+                    return {
+                        text: item.spec_type,
+                        value: item.id
+                    };
+                });
+                state.spec_names = spec_name.objects.map(item => {
+                    return {
+                        text: item.spec_name,
+                        value: item.id,
+                    };
+                });
                 state.isFetching = false;
             });
         });
     }
 
-    handleAddSpec = (e, {value}) => {
+    handleAddLocation = (e, {value}) => {
         this.setState((state, props) => {
-            state.specs.push({value: value, text: value});
+            state.locations.push({value: value, text: value});
+            return state;
+        });
+    };
+    handleAddSpecType = (e, {value}) => {
+        this.setState((state, props) => {
+            state.spec_types.push({value: value, text: value});
+            return state;
+        });
+    };
+    handleAddSpecName = (e, {value}) => {
+        this.setState((state, props) => {
+            state.spec_names.push({value: value, text: value});
             return state;
         });
     };
 
     render() {
-        const {manage_num, spec} = StorageCreateForm.fieldNames;
+        const {manage_num, spec, new_spec, location} = StorageCreateForm.fieldNames;
         return (
             <InteractiveForm reduxFormProps={this.props} loading={this.state.isFetching}>
                 <Segment attached={true}>
                     <FormGroup widths={"equal"}>
                         <Field name={manage_num} component={FieldLazyInput} label="관리번호"/>
                     </FormGroup>
+                    <Field name={location} component={FieldDropDown} label="현재 위치" options={this.state.locations}
+                           onAddItem={this.handleAddLocation}/>
                     <Field name={spec} component={FieldDropDown} label="규격" options={this.state.specs}
                            allowAdditions={false}/>
                     <Divider/>
                     <p>원하는 규격이 없을 경우 아래를 채워 새로 만들 수 있습니다.</p>
-                    <p>새 규격을 만들려면, 기존 규격을 선택하지 말아주세요. 선택시 선택을 우선합니다.</p>
+                    <p>새 규격을 만들려면, 기존 규격을 선택하지 말아주세요. 선택시 기존 규격을 우선합니다.</p>
                     <FormGroup widths={"equal"}>
-                        <Field name={manage_num} component={FieldLazyInput} label="디스크 사양"/>
-                        <Field name={manage_num} component={FieldLazyInput} label="할당단위크기 (GB)"/>
+                        <Field name={new_spec.disk_spec} component={FieldLazyInput} label="디스크 사양"/>
+                        <Field name={new_spec.volume} component={FieldLazyInput} label="할당단위크기 (GB)"/>
                     </FormGroup>
                     <FormGroup widths={"equal"}>
-                        <Field name={spec} component={FieldDropDown} label="형식" options={this.state.specs}
-                               onAddItem={this.handleAddSpec}/>
-                        <Field name={spec} component={FieldDropDown} label="제품명" options={this.state.specs}
-                               onAddItem={this.handleAddSpec}/>
+                        <Field name={new_spec.disk_type} component={FieldDropDown} label="형식"
+                               options={this.state.spec_types}
+                               onAddItem={this.handleAddSpecType}/>
+                        <Field name={new_spec.spec} component={FieldDropDown} label="제품명"
+                               options={this.state.spec_names}
+                               onAddItem={this.handleAddSpecName}/>
                     </FormGroup>
                 </Segment>
                 <Button.Group attached={"bottom"}>
