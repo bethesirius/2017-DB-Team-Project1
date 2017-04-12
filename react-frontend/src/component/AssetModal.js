70358b078b020b991cea5f8ea807c1f7f570e241
elwlwlwk <holo@wisewolf.org>
2017-04-12 23:20:32 +0900

    assetmodal done

--- react-frontend/src/component/AssetModal.js
+++ react-frontend/src/component/AssetModal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Header, Modal} from "semantic-ui-react";
+import {Header, Modal, Dimmer, Loader} from "semantic-ui-react";
 
 class AssetModal extends React.Component {
     static propTypes = {
@@ -9,22 +9,45 @@ class AssetModal extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            assetData: {
-                assetId: 'N00000',
-                assetKind: 'Switch',
-            }
+            assetData: {},
+            isFetching: true,
         };
     }
 
     getData(assetId) {
-        ((assetId) => {
-            this.setState({
-                assetData: {
-                    assetId: assetId,
-                    assetKind: 'Server',
-                }
+        var reqHeaders= new Headers();
+        reqHeaders.append('Content-Type', 'application/json');
+        ((manage_num) => {
+            const filters={
+                filters:[{
+                    name:'manage_num',
+                    op:'eq',
+                    val:manage_num,
+                }]
+            }
+            fetch(`/api/device?q=${JSON.stringify(filters)}`, reqHeaders)
+            .then((r) => r.json())
+            .then((r) => {
+                const device= r.objects[0];
+                return Promise.all([
+                    device,
+                    fetch(`/api/device/${device.id}/asset`,reqHeaders).then((r)=> r.json())
+                ])
             })
-        })(assetId)
+            .then(([device, asset]) => {
+                this.setState({
+                    assetData: {
+                        관리번호: device.manage_num,
+                        구매처: asset.buy_.buy_name,
+                        자산명: asset.asset_name.asset_name,
+                        규격: asset.standard.standard_name,
+                        구입일: asset.get_date,
+                        내용연수: asset.years,
+                    },
+                    isFetching:false,
+                })
+            })
+        })(assetId);
     }
 
     render() {
@@ -35,7 +58,12 @@ class AssetModal extends React.Component {
                    onOpen={(ev, data) => this.getData(assetId)} closeIcon='close'>
                 <Header icon='info' content='Asset Description'/>
                 <Modal.Content>
-                    {Object.keys(assetData).map((key) => <p key={key}>{key}: {assetData[key]}</p>)}
+                    <Dimmer.Dimmable as="div">
+                        <Dimmer active={this.state.isFetching} inverted>
+                            <Loader />
+                        </Dimmer>
+                        {Object.keys(assetData).map((key) => <p key={key}>{key}: {assetData[key]}</p>)}
+                    </Dimmer.Dimmable>
                 </Modal.Content>
             </Modal>
         );
